1537f9e39d3fd5a38f9e675567f033b4a711a29f
Jaime Sainz Argente <raftars77@yahoo.com>
2022-12-02 00:34:03 +0100

    Update day01_part2.py

--- Day01/day01_part2.py
+++ Day01/day01_part2.py
@@ -4,8 +4,5 @@ elves_data = open("input","r").read().strip().split("\n\n")
 elves_data = [sum([int(calorie) for calorie in elf.split("\n")]) for elf in elves_data]
 
 elves_data.sort(reverse=True)
-top3_elves = []
-for i in range(3):
-    top3_elves.append(elves_data[i])
 
-print("Sum of calories from the top 3 elves:",sum(top3_elves))
+print("Sum of calories from the top 3 elves:",sum(elves_data[:3]))
