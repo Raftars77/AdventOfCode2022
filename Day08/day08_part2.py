1cfb4cbf5d59fcab41f5dba1cac78d35257dea31
Raftars77 <raftars77@yahoo.com>
2022-12-09 00:17:53 +0100

    day08_part2.py V2

--- Day08/day08_part2.py
+++ Day08/day08_part2.py
@@ -4,36 +4,21 @@ with open("input","r") as file:
 
 height_map = np.asarray([list(int(tree) for tree in tree_row) for tree_row in input])
 
+movements = [[0,1],[1,0],[0,-1],[-1,0]]
 rows,columns = np.shape(height_map)
-visible_trees = np.zeros((rows,columns)).astype(int)
+visible_trees_map = np.ones((rows,columns)).astype(int)
 
 for row in range(rows):
     for column in range(columns):
-        total_right, total_down, total_up, total_left = 0,0,0,0
-        for tree in range(column+1,columns):
-            if height_map[row, column] > height_map[row, tree]: total_right += 1
-            else:
-                total_right += 1
-                break
-        
-        for tree in range(column-1,-1,-1):
-            if height_map[row, column] > height_map[row, tree]: total_left += 1
-            else:
-                total_left += 1
-                break
+        for movement in movements:
+            visible_trees = 0
+            next_x, next_y = np.array([row,column])+movement
+            while next_x >= 0 and next_x < columns and next_y >= 0 and next_y < rows:
+                if height_map[row, column] > height_map[next_x, next_y]: visible_trees +=1
+                else:
+                    visible_trees += 1
+                    break
+                next_x, next_y = np.array([next_x,next_y])+movement
+            visible_trees_map[row, column]*=visible_trees
 
-        for tree in range(row+1,rows):
-            if height_map[row, column] > height_map[tree, column]: total_down += 1
-            else:
-                total_down += 1
-                break
-
-        for tree in range(row-1,-1,-1):
-            if height_map[row, column] > height_map[tree, column]: total_up += 1
-            else:
-                total_up += 1
-                break
-        
-        visible_trees[row,column] = total_down*total_up*total_left*total_right
-
-print("Highest scenic score possible for any tree:",np.amax(visible_trees))
+print("Highest scenic score possible for any tree:",np.amax(visible_trees_map))
