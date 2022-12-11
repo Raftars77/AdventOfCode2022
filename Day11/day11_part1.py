db1d97c18d92e3f11940972d8a50327d0dfe2066
Raftars77 <raftars77@yahoo.com>
2022-12-11 23:06:03 +0100

    Using lambda function

--- Day11/day11_part1.py
+++ Day11/day11_part1.py
@@ -1,13 +1,6 @@
 import re
-import operator
 with open("input","r") as file:
     input = file.read().strip().split('\n\n')
-
-operator_dicc ={
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul
-}
 
 rounds = 20 #Rounds to be simulated
 monkeys = []
@@ -15,7 +8,7 @@ for monkey in input:
     dict = {}
     _,items,op,div,if_true,if_false = monkey.split('\n')
     dict['items']=[int(item) for item in re.findall(r'\d+',items)]
-    dict['op']=op.split('  Operation: new = old ')[1].split(' ')
+    dict['op']=eval('lambda old:'+op.split('=')[1]) #lambda function for operation
     dict['div']=int(re.findall(r'\d+',div)[0])
     dict['if_true']=int(re.findall(r'\d+',if_true)[0])
     dict['if_false']=int(re.findall(r'\d+',if_false)[0])
@@ -27,7 +20,7 @@ def monkeys_round():
     for monkey in monkeys:
         for _ in range(len(monkey['items'])):
             item = monkey['items'].pop(0)
-            item = operator_dicc[monkey['op'][0]](item,item) if 'old' in monkey['op'] else operator_dicc[monkey['op'][0]](item,int(monkey['op'][1]))
+            item = monkey['op'](item)
             item//=3
             if item%monkey['div']==0: monkeys[monkey['if_true']]['items'].append(item)
             else: monkeys[monkey['if_false']]['items'].append(item)
--- Day11/day11_part2.py
+++ Day11/day11_part2.py
@@ -1,14 +1,7 @@
 import re
-import operator
 from math import lcm
 with open("input","r") as file:
     input = file.read().strip().split('\n\n')
-
-operator_dicc ={
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul
-}
 
 rounds = 10000 #Rounds to be simulated
 monkeys = []
@@ -16,7 +9,7 @@ for monkey in input:
     dict = {}
     _,items,op,div,if_true,if_false = monkey.split('\n')
     dict['items']=[int(item) for item in re.findall(r'\d+',items)]
-    dict['op']=op.split('  Operation: new = old ')[1].split(' ')
+    dict['op']=eval('lambda old:'+op.split('=')[1]) #lambda function for operation
     dict['div']=int(re.findall(r'\d+',div)[0])
     dict['if_true']=int(re.findall(r'\d+',if_true)[0])
     dict['if_false']=int(re.findall(r'\d+',if_false)[0])
@@ -29,9 +22,8 @@ def monkeys_round():
     for monkey in monkeys:
         for _ in range(len(monkey['items'])):
             item = monkey['items'].pop(0)
-            item = operator_dicc[monkey['op'][0]](item,item) if 'old' in monkey['op'] else operator_dicc[monkey['op'][0]](item,int(monkey['op'][1]))
-            #make item value be in the interval [0,lcm]
-            item = item-lcm_div*(item//lcm_div)
+            item = monkey['op'](item)
+            item %= lcm_div #make item value be in the interval [0,lcm]
             if item%monkey['div']==0: monkeys[monkey['if_true']]['items'].append(item)
             else: monkeys[monkey['if_false']]['items'].append(item)
             monkey['inspected_items'] += 1
